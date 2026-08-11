a2b4d558928532cdf8d777aa6c3e7906a28cbecf
david-x3d <david-x3d@users.noreply.github.com>
2026-08-12 00:14:59 +0200

    Release version 1.1.7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.nomi.app"
         minSdk = 26
         targetSdk = 36
-        versionCode = 64
-        versionName = "1.1.6"
+        versionCode = 65
+        versionName = "1.1.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
